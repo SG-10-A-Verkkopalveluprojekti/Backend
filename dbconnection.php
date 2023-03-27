4bdb93f4e027068344327a11e402920c2270b77c
RikuKetomaki <riku.ketomaki@gmail.com>
2023-03-27 12:54:59 +0300

    Added

---
+++ dbconnection.php
@@ -0,0 +1,19 @@
+<?php
+
+function createDbConnection(){
+    $ini = parse_ini_file("myconf.ini");
+
+    $host = $ini["host"];
+    $db = $ini["db"];
+    $username = $ini["username"];
+    $pw = $ini["pw"];
+
+    try{
+    $dbcon = new PDO("mysql:host=$host;dbname=$db", $username, $pw);
+    echo "onnistui";
+    }catch( PDOException $e){
+        echo $e->getMessage();
+    }
+
+    return null;
+}
