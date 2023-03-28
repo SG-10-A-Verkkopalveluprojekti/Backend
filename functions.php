62314d88664c10af316a62df75c6834fc742b897
RikuKetomaki <riku.ketomaki@gmail.com>
2023-03-28 10:10:38 +0300

    Moved $db to functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
 
+function createDbConnection(){
+    $ini = parse_ini_file("myconf.ini");
+
+    $host = $ini["host"];
+    $db = $ini["db"];
+    $username = $ini["username"];
+    $pw = $ini["pw"];
+
+    try{
+    $dbcon = new PDO("mysql:host=$host;dbname=$db", $username, $pw);
+    echo "onnistui";
+    }catch( PDOException $e){
+        echo $e->getMessage();
+    }
+
+    return null;
+}
+
 function returnErr(PDOException $pdoex) {
     header('HTTP/1.1 500 Internal Server Error');
     $error = array('error' => $pdoex->getMessage());
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-require "dbconnection.php";
 require_once "functions.php";
 require_once "headers.php";
 
