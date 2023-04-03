30a779d458c4269ed6140801211957e763b7744a
n2posa00 <n2posa00@students.oamk.fi>
2023-04-03 13:45:29 +0300

    Fixed typo in $uri

--- products/getproduct.php
+++ products/getproduct.php
@@ -3,13 +3,13 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO',PHP_URL_PATH));
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
 $parameters = explode('/',$uri);
 $product_id = $parameters[1];
 
 try {
     $dbcon = createDbConnection();
-    selectRowAsJson($dbcon,"select * from product where id = $product_id");
+    selectRowAsJson($dbcon,"select * from product where product_id = $product_id");
 }
 catch (PDOException $pdoex) {
     returnError($pdoex);
