a2f8b8a83f169693094951c63cead47ee93fcbe1
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-11 14:28:19 +0300

    Changed db to dbcon

--- products/addcategory.php
+++ products/addcategory.php
@@ -7,10 +7,10 @@ $input = json_decode(file_get_contents('php://input'));
 $name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try {
-    $db = createDbConnection();
+    $dbcon = createDbConnection();
     $sql = "insert into category (name) values ('$name')";
-    executeInsert($db,$sql);
-    $data = array('id' => $db->lastInsertId(),'name' => $name);
+    executeInsert($dbcon,$sql);
+    $data = array('id' => $dbcon->lastInsertId(),'name' => $name);
     print json_encode($data);
 }
 catch (PDOException $pdoex) {
