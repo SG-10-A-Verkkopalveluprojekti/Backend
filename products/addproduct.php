eb0e698058bf0163bfd3e0be8a84ff12918e0f96
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-17 11:13:15 +0300

    Fixed database terms

--- products/addproduct.php
+++ products/addproduct.php
@@ -10,9 +10,9 @@ $category_id = filter_var($input->categoryid,FILTER_SANITIZE_FULL_SPECIAL_CHARS)
 
 try {
     $dbcon = createDbConnection();
-    $sql = "insert into product (name,price,image,category_id) values ('$name',$price,'placeholder.png',$category_id)";
+    $sql = "insert into product (name,price,image,category_num) values ('$name',$price,'placeholder.png',$category_id)";
     executeInsert($dbcon,$sql);
-    $data = array('id' => $dbcon->lastInsertId(),'name' => $name,'price' => $price, 'image' => 'placeholder.png');
+    $data = array('category_num' => $dbcon->lastInsertId(),'name' => $name,'price' => $price, 'image' => 'placeholder.png');
     print json_encode($data);
 }
 catch (PDOException $pdoex) {
